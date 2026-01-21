3581a719da7bd981d2d94219d6b85f2b986529c9
Anwesha Pal <anweshapal2006@gmail.com>
2026-01-21 08:37:00 +0530

    Add Maximum Sliding Window implementation in Java
    Implement Maximum Sliding Window algorithm to find maximum values in sliding windows of size k.

---
+++ Maximum_Sliding_Window.java
@@ -0,0 +1,25 @@
+//time complexity: O(n*k)
+//space complexity: O(n-k+1)
+
+import java.util.*;
+class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int []ans=new int[n-k+1];
+        for(int i=0;i<n-k+1;i++){
+            for(int j=i;j<i+k;j++){
+                if(j==i)ans[i]=arr[j];
+                else ans[i]=Math.max(ans[i],arr[j]);
+            }
+        }
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
