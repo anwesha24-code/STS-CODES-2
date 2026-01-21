785098285d31613a4dc176b6a16996637b582a2a
Anwesha Pal <anweshapal2006@gmail.com>
2026-01-21 08:49:41 +0530

    second commit

---
+++ Largest_Possible_Number_Sort_Array_InPlace.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++) a[i] = sc.next();
+
+        Arrays.sort(a, (x, y) -> (y + x).compareTo(x + y));
+        String res = "";
+        for (String s : a)res += s;
+
+        System.out.println(res);
+    }
+}
---
+++ Maximum_Sliding_Window.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int []ans=new int[n-k+1];
+        for(int i=0;i<n;i++){
+            for(int j=i;j<i+k;j++){
+                if(j==i)ans[i]=arr[j];
+                else ans[i]=Math.max(ans[i],arr[j]);
+            }
+        }
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
