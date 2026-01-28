c9554e3d608fd3d770609f828205a0d985526045
Anwesha Pal <anweshapal2006@gmail.com>
2026-01-28 18:06:51 +0530

    Refactor Maximum_Sliding_Window class and logic

--- Maximum_Sliding_Window.java
+++ Maximum_Sliding_Window.java
@@ -1,25 +1,19 @@
 //time complexity: O(n*k)
 //space complexity: O(n-k+1)
-
 import java.util.*;
-class Maximum_Sliding_Window{
-    public static void main(String [] args){
+class Main {
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int k=sc.nextInt();
         int []arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
+        int k=sc.nextInt();
+        for(int i=0;i<n;i++)arr[i]=sc.nextInt();
         int []ans=new int[n-k+1];
         for(int i=0;i<n-k+1;i++){
-            for(int j=i;j<i+k;j++){
-                if(j==i)ans[i]=arr[j];
-                else ans[i]=Math.max(ans[i],arr[j]);
-            }
+            int maxi=Integer.MIN_VALUE;
+            for(int j=i;j<i+k;j++)maxi=Math.max(maxi,arr[j]);
+            ans[i]=maxi;
         }
-        for(int i=0;i<ans.length;i++){
-            System.out.print(ans[i]+" ");
-        }
+        for(int i=0;i<n-k+1;i++)System.out.print(ans[i]+" ");
     }
 }
