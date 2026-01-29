7d45ad3694fbd931964cee1f4eef537b99966273
Anwesha Pal <anweshapal2006@gmail.com>
2026-01-29 10:39:29 +0530

    Update CelebrityProblem.java

--- CelebrityProblem.java
+++ CelebrityProblem.java
@@ -1,40 +1,39 @@
 import java.util.Scanner;
 
 class Main {
-
-    static int n;
-    static int[][] M;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();          // number of people
-        M = new int[n][n];
-        // input matrix
+
+        int n = sc.nextInt();
+        int[][] M = new int[n][n];
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 M[i][j] = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            if (check(i)) {
-                System.out.println(i);
-                return;
+        int celebrity = -1;
+        for (int row = 0; row < n; row++) {
+            boolean ans = true;
+            for (int col = 0; col < n; col++) {
+                if (M[row][col] == 1) {
+                    ans = false;
+                    break;
+                }
+            }
+            if (ans) {
+                for (int i = 0; i < n; i++) {
+                    if (i != row && M[i][row] == 0) {
+                        ans = false;
+                        break;
+                    }
+                }
+            }
+            if (ans) {
+                celebrity = row;
+                break;
             }
         }
 
-        System.out.println(-1);   // no celebrity
-        sc.close();
-    }
-
-    public static boolean check(int c) {
-
-        // row check: celebrity knows no one
-        for (int j = 0; j < n; j++)
-            if (M[c][j] == 1)
-                return false;
-        // column check: everyone knows celebrity
-        for (int i = 0; i < n; i++)
-            if (i != c && M[i][c] == 0)
-                return false;
-        return true;
+        System.out.println(celebrity);
     }
 }
